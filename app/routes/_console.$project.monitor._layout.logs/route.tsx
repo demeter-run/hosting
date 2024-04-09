d0b6dc4d6c001b2d866ab29f7de483df02ca9eb3
Rodre <rodrigo@txpipe.io>
2024-04-08 23:56:35 -0300

    implemented logs refresh button

--- app/routes/_console.$project.monitor._layout.logs/route.tsx
+++ app/routes/_console.$project.monitor._layout.logs/route.tsx
@@ -1,54 +1,56 @@
-import { json, type MetaFunction } from '@remix-run/node';
-// import { useState } from 'react';
-// import { useLoaderData } from '@remix-run/react';
-import invariant from '~/helpers/invariant';
+import { ActionFunctionArgs, json, type MetaFunction } from '@remix-run/node';
+import { useFetcher } from '@remix-run/react';
+import { useEffect, useMemo, useState } from 'react';
+import { PageLoader } from '~/fragments/page-loader';
 import { getLogs } from '~/server/logs.server';
 
 export const meta: MetaFunction = () => {
     return [{ title: 'Logs - Demeter Hosting' }, { name: 'description', content: 'Logs - Demeter Hosting' }];
 };
 
-export async function loader() {
-    const logs = getLogs();
-
-    invariant(logs, 'Failed to load logs data');
-
+export async function action({ request }: ActionFunctionArgs) {
+    const logs = await getLogs();
     return json({ logs });
 }
 
-const DATA = [
-    '2024-03-14T13:54:42.338827319Z HEAD /backups/directory.zip 404 - - 1.080 ms\n',
-    '2024-03-14T13:55:40.707799067Z HEAD /backups/www.rar 404 - - 1.142 ms\n',
-    '2024-03-14T13:56:12.242978036Z HEAD /old/config.js 404 - - 2.140 ms\n',
-    '2024-03-14T14:01:13.159746374Z HEAD /back/directory.zip 404 - - 2.852 ms\n',
-    '2024-03-14T14:05:28.585610352Z HEAD /restore/.env 404 - - 1.762 ms\n',
-    '2024-03-14T14:06:26.362239326Z GET /robots.txt 404 - - 1.105 ms\n',
-    '2024-03-14T14:08:59.375340025Z HEAD /back/public_html.zip 404 - - 1.256 ms\n',
-    '2024-03-14T14:13:01.289019831Z HEAD /restore/bak.rar 404 - - 1.213 ms\n',
-    '2024-03-14T14:13:14.321920362Z HEAD /restore/Archive.zip 404 - - 2.287 ms\n',
-    'HEAD /backup/backup.gz 404 - - 2.305 ms\n',
-    '2024-03-14T14:15:01.709886876Z HEAD /back/index.zip 404 - - 1.080 ms\n',
-    '2024-03-14T14:22:21.996883172Z HEAD /backup/public_html.tar.gz 404 - - 1.710 ms\n',
-    '2024-03-14T14:35:07.240209363Z HEAD /backups/txpipe.io.zip 404 - - 1.100 ms\n',
-];
-
 export default function Logs() {
-    // const { logs } = useLoaderData<typeof loader>();
-    // const [wrap, setWrap] = useState(false);
+    const fetcher = useFetcher();
+    const [output, setOutput] = useState('');
     const wrap = false;
+    const fetcherRunning = useMemo(() => fetcher.state === 'loading' || fetcher.state === 'submitting', [fetcher.state]);
 
-    const output = DATA.map(log => log.replace(/ /g, '&nbsp').replace(/\n/g, '<br />')).join('');
+    useEffect(() => {
+        if (fetcher.data) {
+            const newOutput = (fetcher.data as { logs: string[] }).logs
+                .map((log: string) => log.replace(/ /g, '&nbsp').replace(/\n/g, '<br />'))
+                .join('');
+            setOutput((prev: string) => prev.concat(newOutput));
+        }
+    }, [fetcher.data]);
+
+    function handleRefresh() {
+        fetcher.submit({}, { method: 'POST' });
+    }
+
+    useEffect(() => {
+        handleRefresh();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <>
+            {fetcherRunning && <PageLoader />}
             <h1 className="title-3xl">Logs</h1>
 
             <div
-                className={`bg-gray-950 mt-4 p-4 rounded-md h-[calc(100vh-240px)] font-mono text-sm overflow-x-auto text-gray-400 ${
+                className={`bg-gray-950 mt-4 p-4 rounded-md h-[calc(100vh-300px)] font-mono text-sm overflow-x-auto text-gray-400 ${
                     wrap ? 'whitespace-normal' : 'whitespace-nowrap'
                 } `}
                 dangerouslySetInnerHTML={{ __html: output || 'loading' }}
-            ></div>
+            />
+            <button className="btn-primary mt-4" onClick={handleRefresh}>
+                Refresh
+            </button>
         </>
     );
 }
