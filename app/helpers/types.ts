e5ac5206529d6b1ec8c192880814c55fbf6768f7
Rodre <rodrigo@txpipe.io>
2024-04-11 01:01:52 -0300

    updated activity loader

--- app/helpers/types.ts
+++ app/helpers/types.ts
@@ -1,27 +1,34 @@
 export type Namespace = {
-  name: string;
-  address: string;
-  policyId: string;
-  timestamp: string | number;
-  hash: string;
+    name: string;
+    address: string;
+    policyId: string;
+    timestamp: string | number;
+    hash: string;
 };
 
 export type gasLedgerItem = {
-  id: number;
-  timestamp: string | number;
-  provider?: string;
-  dcus: number;
-  requests?: number;
-  hash: string;
-  type: string;
+    id: number;
+    timestamp: string | number;
+    provider?: string;
+    dcus: number;
+    requests?: number;
+    hash: string;
+    type: string;
 };
 
 export type Provider = {
-  id: number;
-  name: string;
-  location: string;
-  logo: string;
-  features?: string[];
-  supportLink?: string;
-  isEnabled: boolean;
+    id: number;
+    name: string;
+    location: string;
+    logo: string;
+    features?: string[];
+    supportLink?: string;
+    isEnabled: boolean;
+};
+
+export type ActivityItem = {
+    id: number;
+    timestamp: string | number;
+    requests: number;
+    dcus: number;
 };
--- app/routes/_console.$project.monitor._layout.activity/route.tsx
+++ app/routes/_console.$project.monitor._layout.activity/route.tsx
@@ -2,22 +2,21 @@ import { json, type MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { formatDate, formatDateClient } from '~/helpers/date';
 import invariant from '~/helpers/invariant';
-import { getCosts } from '~/server/activity.server';
 import { LineChart, Line, ResponsiveContainer, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { getPageData } from '~/server/activity.server';
 
 export const meta: MetaFunction = () => {
     return [{ title: 'Cost - Demeter Hosting' }, { name: 'description', content: 'Cost - Demeter Hosting' }];
 };
 
 export async function loader() {
-    const costs = await getCosts();
-    invariant(costs, 'Failed to load costs data');
-
-    return json({ costs });
+    const pageData = await getPageData();
+    invariant(pageData, 'Failed to load page data');
+    return json({ pageData });
 }
 
 export default function Activity() {
-    const { costs } = useLoaderData<typeof loader>();
+    const { pageData: pd } = useLoaderData<typeof loader>();
 
     return (
         <>
@@ -27,7 +26,7 @@ export default function Activity() {
                 <div className="content-wrapper p-4">
                     <div className="font-mono font-semibold mb-4">Requests</div>
                     <ResponsiveContainer width="100%" height={200}>
-                        <LineChart data={costs} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+                        <LineChart data={pd.activity} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
                             <Line type="linear" dataKey="requests" stroke="#4f46e5" />
                             <CartesianGrid stroke="#e6e6e6" strokeDasharray="5 5" />
                             <XAxis
@@ -51,7 +50,7 @@ export default function Activity() {
                 <div className="content-wrapper p-4">
                     <div className="font-mono font-semibold mb-4">DCUS</div>
                     <ResponsiveContainer width="100%" height={200}>
-                        <LineChart data={costs} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+                        <LineChart data={pd.activity} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
                             <Line type="linear" dataKey="dcus" stroke="#4f46e5" />
                             <CartesianGrid stroke="#e6e6e6" strokeDasharray="5 5" />
                             <XAxis
@@ -83,11 +82,11 @@ export default function Activity() {
                         </tr>
                     </thead>
                     <tbody className="table-body">
-                        {costs.map(c => (
-                            <tr key={c.id} className="table-tr">
-                                <td className="table-td">{formatDateClient(new Date(c.timestamp), 'medium', 'short')}</td>
-                                <td className="table-td text-right">{c.requests.toLocaleString('en-US')}</td>
-                                <td className="table-td text-right">{c.dcus.toLocaleString('en-US')}</td>
+                        {pd.activity.map(a => (
+                            <tr key={a.id} className="table-tr">
+                                <td className="table-td">{formatDateClient(new Date(a.timestamp), 'medium', 'short')}</td>
+                                <td className="table-td text-right">{a.requests.toLocaleString('en-US')}</td>
+                                <td className="table-td text-right">{a.dcus.toLocaleString('en-US')}</td>
                             </tr>
                         ))}
                     </tbody>
