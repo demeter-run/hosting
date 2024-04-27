f4d9ee6a0d41ff2e319bb2bfce9dd1c5d9fabe1d
Rodre <rodrigo@txpipe.io>
2024-04-26 21:01:47 -0300

    updated mint flow UI

--- app/fragments/footer.tsx
+++ app/fragments/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="wrapper">
-            <div className="flex items-center py-10">
+            <div className="flex items-center h-20">
                 <img className="w-6" src="/assets/logos/demeter-isotype-black.svg" alt="Demeter" />
                 <p className="text-sm ml-4">© 2024</p>
             </div>
